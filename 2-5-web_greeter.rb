63ec4a4b312f11440ca4bfb515e761e48e42e9c0
spxds <mactechy@gmail.com>
2015-10-06 14:52:01 -0400

    Delete 2-5-web_greeter.rb

--- 2-5-web_greeter.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-
-get '/:name' do
-  name = params[:name]
-  "Hello #{name}! Did you know that your name has #{name.length} letters in it and written backwards it is '#{name.reverse.downcase.capitalize}'?"
-end
