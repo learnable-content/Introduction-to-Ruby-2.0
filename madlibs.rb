22b9ed2a8a36502985b03e48f1698fbc13b14546
Melissa <mactechy@gmail.com>
2015-10-06 14:51:09 -0400

    added code

---
+++ madlibs.rb
@@ -0,0 +1,13 @@
+puts "Please enter an animal:"
+animal = gets.chomp
+puts "Please enter a color:"
+color = gets.chomp
+puts "Please enter an type of person:"
+person = gets.chomp
+puts "Please enter an object:"
+object = gets.chomp
+puts "Please enter an adjective:"
+adjective = gets.chomp
+puts "Please enter a verb:"
+verb = gets.chomp
+puts "The #{adjective} #{animal} started to #{verb} because the #{person} ran away with the #{color} #{object}"
