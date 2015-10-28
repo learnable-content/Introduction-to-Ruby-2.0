8981bad6b7eb572d34e738a961f57302da0884b1
melissa <altercube@gmail.com>
2015-10-28 00:15:32 -0400

    no message

--- greeter.rb
+++
@@ -1,3 +0,0 @@
-puts "Please enter your name:"
-name = gets.chomp
-puts "Hello #{name}! Did you know that your name has #{name.length} letters in it and written backwards it is '#{name.reverse.downcase.capitalize}'?"
